43d1706411ddb1ad2da1a14070efe175ef6ea4c3
Filipe <Filipecasagrande1@gmail.com>
2025-03-20 20:35:01 -0300

    Implementada função if

--- JogoDeAdivinhação/Program.cs
+++ JogoDeAdivinhação/Program.cs
@@ -4,18 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Jogo Da Adivinhação");
-            Console.WriteLine("----------------------------------------");
-
-            Console.Write("Digite um número para Chutar: ");
-            int Numerochute = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Você dugitou o numero: " + Numerochute);
+            while (true)
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Jogo Da Adivinhação");
+                Console.WriteLine("----------------------------------------");
 
 
+                Random gdn = new Random();
+                int ns = gdn.Next(1, 21);
 
-            Console.ReadLine(); 
+                Console.Write("Digite um número (de 1 à 20 )para Chutar: ");
+                int Numerochute = Convert.ToInt32(Console.ReadLine());
+
+                if (Numerochute == ns)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Parabéns você acertou! ");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else if (Numerochute > ns)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O numero digitado foi maior que o número secreto!");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else  
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O numero digitado foi Menor que o número secreto!");
+                    Console.WriteLine("----------------------------------------");
+                }
+
+
+
+
+
+                    Console.ReadLine();
+
+
+
+            }
         }
     }
 }
