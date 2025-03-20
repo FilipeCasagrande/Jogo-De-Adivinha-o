84ae96dd4c22b351e04e01d20264896322d4e3c6
Filipe <Filipecasagrande1@gmail.com>
2025-03-20 20:07:24 -0300

    Implementava versão 1 com entrada e saia de dados

--- JogoDeAdivinhação/Program.cs
+++ JogoDeAdivinhação/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Jogo Da Adivinhação");
+            Console.WriteLine("----------------------------------------");
+
+            Console.Write("Digite um número para Chutar: ");
+            int Numerochute = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Você dugitou o numero: " + Numerochute);
+
+
+
+            Console.ReadLine(); 
         }
     }
 }
