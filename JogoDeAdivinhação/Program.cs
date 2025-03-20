f81327b5ff40f666e03973c07496af4319fe791b
Filipe <Filipecasagrande1@gmail.com>
2025-03-20 19:55:14 -0300

    Add project files.

---
+++ JogoDeAdivinhação/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoDeAdivinhação
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
